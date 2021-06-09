04061c997f20c2a6a71c7bf0f3bbca57c470601f
teddy arada <teddy.shibabaw@yahoo.com>
2021-06-08 20:46:53 -0700

    addedd NetflixClone

---
+++ Netflix-clone-master/build/precache-manifest.f3d982a35b36bc14d31b729e69be8e5b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e2816d5c4904e86abe516948ae39b3c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "336b203628fa6c43c7b0",
+    "url": "/static/css/main.b44b9242.chunk.css"
+  },
+  {
+    "revision": "e6dc09567efd60452578",
+    "url": "/static/js/2.fc130dab.chunk.js"
+  },
+  {
+    "revision": "894f61387b383e6e2e0543fb120b4656",
+    "url": "/static/js/2.fc130dab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "336b203628fa6c43c7b0",
+    "url": "/static/js/main.870fd674.chunk.js"
+  },
+  {
+    "revision": "07b33697149324db54e5",
+    "url": "/static/js/runtime-main.b8a87127.js"
+  }
+]);
